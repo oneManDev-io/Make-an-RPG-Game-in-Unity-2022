773e16f4b387c233d7e145151625b0519adccf01
Abid Al Wassie <abidalwassie@outlook.com>
2022-10-18 18:27:41 +0600

    fixed lesson 4

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,26 +4,59 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public enum PlayerState
+    {
+        walk
+    }
+
+    public PlayerState currentState;
+
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
 
     Vector2 velocity;
 
+    public Animator animator;
+
+    // Start is called before the first frame update
     void Start()
     {
+        moveSpeed = 5f;
+        currentState = PlayerState.walk;
+        velocity = Vector2.down;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (currentState == PlayerState.walk)
+        {
+            UpdateAnimation();
+        }
+
         velocity = Vector2.zero;
         velocity.x = Input.GetAxisRaw("Horizontal");
         velocity.y = Input.GetAxisRaw("Vertical");
-        Debug.Log(velocity);
     }
 
-    void FixedUpdate()
+    void UpdateAnimation()
+    {
+        if (velocity != Vector2.zero)
+        {
+            UpdateMovement();
+
+            animator.SetBool("Running", true);
+            animator.SetFloat("Horizontal", velocity.x);
+            animator.SetFloat("Vertical", velocity.y);
+        }
+        else
+        {
+            animator.SetBool("Running", false);
+        }
+    }
+
+
+    void UpdateMovement()
     {
         rb.MovePosition(rb.position + velocity * moveSpeed * Time.fixedDeltaTime);
     }
